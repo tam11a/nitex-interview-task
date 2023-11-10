4b153a9573d15aafd9c99be2308b7b09e77432e4
tam11a <ibrahimsadiktamim@gmail.com>
2023-11-11 00:42:48 +0600

    completed: sign up and signout ui and order table

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,36 +1,144 @@
 import useAuth from "@/hooks/useAuth";
+import { clean } from "@/store/auth";
 import Iconify from "@components/iconify";
-import { Avatar, IconButton } from "@mui/material";
-import { Tooltip } from "antd";
+import { Avatar, AvatarGroup, IconButton } from "@mui/material";
+import { Collapse, Tooltip } from "antd";
 import React from "react";
+import { useDispatch } from "react-redux";
 
 const Dashboard: React.FC = () => {
 	const { user } = useAuth();
 
 	const orders = JSON.parse(localStorage.getItem("orders") || "[]") || [];
 
-	console.log(orders);
+	const dispatch = useDispatch();
 
 	return (
-		<div>
+		<div className="max-w-5xl mx-auto">
+			<p className="font-bold text-2xl mb-4">My Account</p>
 			<div className="flex flex-row gap-3 items-center p-3 bg-slate-100 rounded-md">
 				<Avatar
 					src={user?.photoURL}
 					variant="rounded"
 				/>
 				<div className="flex flex-col flex-1">
-					<h1 className="font-bold">{user?.displayName}</h1>
-					<h2 className="text-sm">{user?.email}</h2>
+					<h1 className="font-bold">
+						{user?.displayName}{" "}
+						<span className="hidden sm:inline">
+							&bull;{" "}
+							<span className="text-slate-500 text-sm">
+								Signed in with Google
+							</span>
+						</span>
+					</h1>
+					<h2 className="text-sm text-slate-600">{user?.email}</h2>
 				</div>
 				<div>
 					<Tooltip title="Logout">
-						<IconButton>
+						<IconButton
+							onClick={() => {
+								dispatch(clean());
+							}}
+						>
 							<Iconify icon="mdi:logout" />
 						</IconButton>
 					</Tooltip>
 				</div>
 			</div>
-			<div></div>
+			<p className="font-bold text-2xl my-4 mt-8">My Orders</p>
+			{orders?.length === 0 ? (
+				<div>
+					<p className="text-slate-500 max-w-sm text-center mx-auto my-32">
+						You have no orders yet. Please order some products to see them here.
+					</p>
+				</div>
+			) : (
+				<div>
+					<Collapse
+						expandIcon={() => <></>}
+						bordered={false}
+						items={Array.from(orders || [], (item: any, index: number) => {
+							let totalQt = 0;
+							let totalPrice = 0;
+
+							item?.products?.forEach((product: any) => {
+								totalQt += product?.qt;
+								totalPrice += product?.product?.price * product?.qt;
+							});
+
+							return {
+								key: index,
+								label: (
+									<div
+										className="grid grid-cols-2
+								md:grid-cols-3 gap-3"
+									>
+										<div>
+											<b>{item.receipent.name}</b>
+											<p>+880 {item.receipent.phone}</p>
+										</div>
+										<div>
+											<AvatarGroup
+												total={item?.products?.length}
+												className="mr-3 float-left"
+											>
+												{item?.products?.map?.(
+													(product: any, index: number) => (
+														<Avatar
+															alt={product?.product?.title}
+															src={product?.product?.thumbnail}
+															key={index}
+														/>
+													)
+												)}
+											</AvatarGroup>
+											<div>
+												<p>{item?.products?.length || 0} Products</p>
+												<p className="font-bold">{totalQt} Items</p>
+											</div>
+										</div>
+										<div className="col-span-2 md:col-span-1">
+											<p className="text-right">
+												Total: <b>${totalPrice}</b>
+											</p>
+										</div>
+									</div>
+								),
+								children: (
+									<div className="bg-white p-3 rounded flex flex-col gap-3">
+										{item?.products?.map?.((product: any, index: number) => (
+											<div
+												className="flex flex-row gap-3"
+												key={index}
+											>
+												<div>
+													<Avatar
+														variant="rounded"
+														className="mt-1"
+														alt={product?.product?.title}
+														src={product?.product?.thumbnail}
+													/>
+												</div>
+												<div>
+													<p className="font-bold">{product?.product?.title}</p>
+													<p className="text-sm text-slate-600">
+														{product?.product?.description}
+													</p>
+													<p className="text-sm text-slate-600">
+														${product?.product?.price} x{" "}
+														<b>{product?.qt} items</b> ={" "}
+														<b>${product?.product?.price * product?.qt}</b>
+													</p>
+												</div>
+											</div>
+										))}
+									</div>
+								),
+							};
+						})}
+					/>
+				</div>
+			)}
 		</div>
 	);
 };
--- src/pages/Sign/index.tsx
+++ src/pages/Sign/index.tsx
@@ -35,8 +35,36 @@ const Sign: React.FC = () => {
 	const { isLoggedIn } = useAuth();
 
 	return !isLoggedIn ? (
-		<div>
-			<button onClick={signInWithGoogle}>Sign in with Google</button>
+		<div className="m-6 py-10">
+			<h1 className="text-2xl font-bold text-center">😇 Let's get Started</h1>
+			<p className="text-center my-2 font-semibold text-slate-600">
+				Sign up to the family and get started immediately
+			</p>
+			<div className="w-fit mx-auto mt-8">
+				<button
+					type="button"
+					className="text-secondary w-full  bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center justify-between dark:focus:ring-[#4285F4]/55 mr-2 mb-2"
+					onClick={signInWithGoogle}
+				>
+					<svg
+						className="mr-2 -ml-1 w-4 h-4"
+						aria-hidden="true"
+						focusable="false"
+						data-prefix="fab"
+						data-icon="google"
+						role="img"
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 488 512"
+					>
+						<path
+							fill="currentColor"
+							d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"
+						></path>
+					</svg>
+					<span className="text-white">Sign up with Google</span>
+					<div></div>
+				</button>
+			</div>
 		</div>
 	) : (
 		<Navigate to={searchParams.get("redirect") || "/app/user"} />
